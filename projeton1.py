c889c510442e7e3c56b624ce90c1eafc64370f27
filipoow <emaildofilipe.m@gmail.com>
2021-09-28 11:56:35 -0300

    Adicionando sistema de escolha
    Utilização do if para sistema de escolha.
    
    Co-Authored-By: Breno Perondi Sater <82115545+brenopsps@users.noreply.github.com>

--- projeton1.py
+++ projeton1.py
@@ -3,3 +3,28 @@ print("Breno Perondi - 21538372")
 print("Filipe Martins - 21538237")
 print("Gustavo Souza da Silva - 21572052")
 print("Lucas Rodrigues de Andrade Segatti - 21570701")
+
+#Cadastro de novo usuário
+print("1 - Cadastro de Usuário")
+print("2 - Exibir todos os cadastros(Ordem de Cadastro)")
+print("3 - Exibir todos os cadastros(Ordem de Alfabética)")
+print("4 - Verificação")
+print("5 - Remover usuário")
+print("6 - Alterar nome de um usuário")
+op = input("Qual será sua operação?")
+
+if op == 1:
+    print("-------------------")
+    print("Cadastro de Usuário")
+    nome = input("Nome:")
+    email = input("E-mail:")
+elif op == 2:
+    print("Exibir todos os cadastros(Ordem de Cadastro)")
+elif op == 3:
+    print("Exibir todos os cadastros(Ordem de Alfabética)")
+elif op == 4:
+    print("Verificação")
+elif op == 5:
+    print("Remover usuário")
+elif op == 6:
+    print("Alterar nome de um usuário")
