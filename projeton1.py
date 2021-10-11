657b2966edf7e30fb04c48ae291c91928ee71032
filipoow <emaildofilipe.m@gmail.com>
2021-10-11 18:25:54 -0300

    Alterações no sistema de cadastro.
    Co-Authored-By: Breno Perondi Sater <82115545+brenopsps@users.noreply.github.com>

--- projeton1.py
+++ projeton1.py
@@ -4,17 +4,39 @@ from time import sleep
 print("-=" *30)
 sleep(2)
 print('Bem-vindo ao sistema de cadastro, desenvolvido pelos alunos: ')
+
+#Sistema de login
 print("-=" *30)
-# Funções
-def projeton1():
-    print("-=" *30)
-    user = input("Digite seu login de administrador: ")
-    password = input("Digite sua senha: ")
-    if user and password == "AaDdMm":
+office = input("Digite seu cargo: ")
+user = input("Digite seu login: ")
+password = input("Digite sua senha: ")
+if office == "Gestor":
+    if user and password == "Adm":
         sleep(2)
         print("Entrando no sistema...")
+        print("-=" *30)
+        ver = True
+else:
+    print("-=" *30)
+    print("Seu usuário não tem acesso as funções de gestor.")
+    ver = False
+
+# Funções
+
+def projeton1():
+    print("-=" *30)
+    office = input("Digite seu cargo: ")
+    user = input("Digite seu login: ")
+    password = input("Digite sua senha: ")
+    if office == "Gestor":
+        if user and password == "Adm":
+            sleep(2)
+            print("Entrando no sistema...")
+    else:
+        print("-=" *30)
+        print("Seu usuário não tem acesso as funções de gestor.")
     
-    
+
 def usuariosCadastrados():
     print("-=" *30)
     print("Os usuários cadastrados são: ")
@@ -41,19 +63,25 @@ def buscandoUsuario(nome):
 
 # Programa principal
 
-cadastroUsuarios = []
+if ver == True:
+    cadastroUsuarios = []
 
-while True:
-    cadastroUsuarios.append(input("Digite o nome completo do usuário: "))
-    cadastroUsuarios.append(input("Digite o e-mail do usuário: "))
-    continuar = input("Deseja continuar cadastrando mais usuários? S/N: ")
-    while continuar not in 'SsNn':
-        continuar = input('Resposta inválida, deseja continuar? S/N: ')
-    if continuar in 'Nn':
-        break
+    while True:
+        cadastroUsuarios.append(input("Digite o nome completo do usuário: "))
+        cadastroUsuarios.append(input("Digite o e-mail do usuário: "))
+        continuar = input("Deseja continuar cadastrando mais usuários? S/N: ")
+        while continuar not in 'SsNn':
+            continuar = input('Resposta inválida, deseja continuar? S/N: ')
+        if continuar in 'Nn':
+            break
+    usuariosCadastrados()
+    usuariosPorOrdemAlfabetica()
+    print("-=" *30)
+    buscandoUsuario(input("Digite o nome que você deseja encontrar na lista: "))
+else:
+    projeton1()
 
-projeton1()
-usuariosCadastrados()
-usuariosPorOrdemAlfabetica()
-print("-=" *30)
-buscandoUsuario(input("Digite o nome que você deseja encontrar na lista: "))
+
+
+
+
