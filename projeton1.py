310e25e251bfb00d06f0cc3f7dd164f13398c20e
filipoow <emaildofilipe.m@gmail.com>
2021-10-11 16:51:56 -0300

    Mudaças nas defs e criação de login
    Criando um sistema de login para o gestor.
    
    Co-Authored-By: Allan Yamada <88389192+ALYamada@users.noreply.github.com>
    Co-Authored-By: Breno Perondi Sater <82115545+brenopsps@users.noreply.github.com>

--- projeton1.py
+++ projeton1.py
@@ -1,30 +1,59 @@
-print("Allan Yamada - 21516669")
-print("Breno Perondi - 21538372")
-print("Filipe Martins - 21538237")
-print("Gustavo Souza da Silva - 21572052")
-print("Lucas Rodrigues de Andrade Segatti - 21570701")
+from time import sleep
 
-#Cadastro de novo usuário
-print("1 - Cadastro de Usuário")
-print("2 - Exibir todos os cadastros(Ordem de Cadastro)")
-print("3 - Exibir todos os cadastros(Ordem de Alfabética)")
-print("4 - Verificação")
-print("5 - Remover usuário")
-print("6 - Alterar nome de um usuário")
-op = input("Qual será sua operação?")
 
-if op == 1:
-    print("-------------------")
-    print("Cadastro de Usuário")
-    nome = input("Nome:")
-    email = input("E-mail:")
-elif op == 2:
-    print("Exibir todos os cadastros(Ordem de Cadastro)")
-elif op == 3:
-    print("Exibir todos os cadastros(Ordem de Alfabética)")
-elif op == 4:
-    print("Verificação")
-elif op == 5:
-    print("Remover usuário")
-elif op == 6:
-    print("Alterar nome de um usuário")
+print("-=" *30)
+sleep(2)
+print('Bem-vindo ao sistema de cadastro, desenvolvido pelos alunos: ')
+print("-=" *30)
+# Funções
+def projeton1():
+    print("-=" *30)
+    user = input("Digite seu login de administrador: ")
+    password = input("Digite sua senha: ")
+    if user and password == "AaDdMm":
+        sleep(2)
+        print("Entrando no sistema...")
+    
+    
+def usuariosCadastrados():
+    print("-=" *30)
+    print("Os usuários cadastrados são: ")
+    sleep(0.5)
+    print(cadastroUsuarios)
+
+def usuariosPorOrdemAlfabetica():
+    print("-=" *30)
+    print("Os usuários em ordem alfabética são: ")
+    sleep(0.5)
+    cadastroUsuarios.sort()
+    for usuariosAlfabetico in cadastroUsuarios:
+        print(usuariosAlfabetico)
+
+
+def buscandoUsuario(nome):
+    print("-=" *30)
+    print("Buscando um usuário pelo seu nome...")
+    sleep(1)
+    if nome in cadastroUsuarios:
+        print(f"Foi encontrado na lista o nome {nome}")
+    else:
+        print(f"Não foi encontrado na lista o nome {nome}")
+
+# Programa principal
+
+cadastroUsuarios = []
+
+while True:
+    cadastroUsuarios.append(input("Digite o nome completo do usuário: "))
+    cadastroUsuarios.append(input("Digite o e-mail do usuário: "))
+    continuar = input("Deseja continuar cadastrando mais usuários? S/N: ")
+    while continuar not in 'SsNn':
+        continuar = input('Resposta inválida, deseja continuar? S/N: ')
+    if continuar in 'Nn':
+        break
+
+projeton1()
+usuariosCadastrados()
+usuariosPorOrdemAlfabetica()
+print("-=" *30)
+buscandoUsuario(input("Digite o nome que você deseja encontrar na lista: "))
